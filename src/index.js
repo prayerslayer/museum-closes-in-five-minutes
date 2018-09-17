94f83c2488dc8bb73b2c22a10fed89d09f461159
Nikolaus Piccolotto <nikolaus.piccolotto@artory.com>
2018-09-17 21:44:24 +0200

    A shit ton of things work

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import Phaser from "phaser";
 
-import SPRITES from "./assets/img/exit_game.png";
+// import SPRITES from "./assets/img/exit_game.png";
+import PLAYER from "./assets/img/walk_right.png";
+import WALLS from "./assets/img/walls.png";
 import MUSEUM from "./assets/map/room.csv";
+import MONET from "./assets/img/paintings/Monet_Nympheas_1904.png";
+import MUNCH from "./assets/img/paintings/Munch_-_The_Scream.png";
+
+const paintings = [MONET, MUNCH];
 
 var config = {
   type: Phaser.AUTO,
-  width: 320,
-  height: 320,
+  width: 640,
+  height: 640,
   pixelArt: true,
   physics: {
     default: "arcade",
     arcade: {
-      gravity: false,
-      debug: true
+      gravity: false
+      // debug: true
     }
   },
   scene: {
@@ -32,21 +38,37 @@ var cursors;
 var map;
 
 function preload() {
-  this.load.spritesheet("tiles", SPRITES, {
-    frameWidth: 10,
-    frameHeight: 10
+  this.load.spritesheet("player", PLAYER, {
+    frameWidth: 16,
+    frameHeight: 16
   });
+  this.load.spritesheet("walls", WALLS, {
+    frameWidth: 16,
+    frameHeight: 16
+  });
+  paintings.forEach(painting => this.load.image(painting, painting));
   this.load.tilemapCSV("museum", MUSEUM);
 }
 
 function updatePlayer() {
+  const anyDown =
+    cursors.left.isDown ||
+    cursors.right.isDown ||
+    cursors.up.isDown ||
+    cursors.down.isDown;
+
   if (cursors.left.isDown) {
     player.setVelocityX(-80);
+    player.anims.play("walk_left", true);
   } else if (cursors.right.isDown) {
     player.setVelocityX(80);
+    player.anims.play("walk_right", true);
   } else {
     player.setVelocityX(0);
   }
+
+  player.depth = player.y;
+
   if (cursors.up.isDown) {
     player.setVelocityY(-80);
   } else if (cursors.down.isDown) {
@@ -54,14 +76,18 @@ function updatePlayer() {
   } else {
     player.setVelocityY(0);
   }
+
+  if (!anyDown) {
+    player.anims.play("idle");
+  }
 }
 
 function checkOverlapWithPainting() {
   const tile = layer.getTileAtWorldXY(player.x, player.y);
-  if (tile.index === DOOR_TILE && !tile.properties.visited) {
+  if (tile.index === PAINTING_TILE && !tile.properties.visited) {
     tile.properties.visited = true;
     score += 1;
-    map.replaceByIndex(tile.index, GROUND_TILE, tile.x, tile.y, 1, 1);
+    tile.properties.image.setTint(0xaaaaaa);
   }
 }
 
@@ -73,17 +99,25 @@ function moveVisitors() {
   const now = Date.now();
   visitors.children.iterate(v => {
     if (now - v.moved > 3000) {
-      v.setVelocityX(0);
-      v.setVelocityY(0);
       const direction = Phaser.Math.RND.weightedPick([0, 1, 2, 3, 4, 5, 6, 7]);
-      if (direction !== 0) {
+      if (direction >= 4) {
         const xDir = direction == 4 ? -1 : direction === 5 ? 1 : 0;
         const yDir = direction == 6 ? -1 : direction === 7 ? 1 : 0;
         v.setVelocityX(20 * xDir);
         v.setVelocityY(20 * yDir);
+        if (xDir > 0) {
+          v.anims.play("walk_right", true);
+        } else if (xDir < 0) {
+          v.anims.play("walk_left", true);
+        }
         v.moved = Date.now();
+      } else {
+        v.setVelocityX(0);
+        v.setVelocityY(0);
+        v.anims.play("idle", true);
       }
     }
+    v.depth = v.y;
   });
 }
 
@@ -94,25 +128,31 @@ function update() {
   moveVisitors();
 }
 
-const DOOR_TILE = 4;
-const GROUND_TILE = 1;
-const WALL_TILE = 0;
+const WALL_TILE = 5;
+const PAINTING_TILE = 6;
 
 function create() {
   cursors = this.input.keyboard.createCursorKeys();
-  map = this.add.tilemap("museum", 10, 10, 512, 512);
+  map = this.make.tilemap({ key: "museum", tileWidth: 16, tileHeight: 16 });
   map.setCollision([WALL_TILE]);
-  const tileset = map.addTilesetImage("tiles");
-  layer = map.createDynamicLayer(0, tileset, 0, 0);
+  const tileset = map.addTilesetImage("walls");
+  layer = map.createStaticLayer(0, tileset, 0, 0);
+  let lastUsedPainting = -1;
   layer.forEachTile(tile => {
-    if (tile.index === DOOR_TILE) {
+    if (tile.index === PAINTING_TILE) {
+      const painting = lastUsedPainting + 1;
       tile.properties.visited = false;
+      const { x, y } = layer.tileToWorldXY(tile.x, tile.y);
+      const img = this.add.image(x + 8, y + 8, paintings[painting]);
+      img.setOrigin(1, 1);
+      tile.properties.image = img;
+      lastUsedPainting = painting;
     }
   });
 
   visitors = this.physics.add.group({
-    defaultKey: "tiles",
-    defaultFrame: 3,
+    defaultKey: "player",
+    defaultFrame: 0,
     frameQuantity: 500
   });
   for (let i = 0; i < 100; i++) {
@@ -120,15 +160,39 @@ function create() {
     do {
       x = Math.floor(Math.random() * 400);
       y = Math.floor(Math.random() * 400);
-    } while (layer.getTileAtWorldXY(x, y).index !== GROUND_TILE);
+    } while (layer.getTileAtWorldXY(x, y).index === WALL_TILE);
     visitors.get(x, y);
   }
-  visitors.children.iterate(v => (v.moved = Date.now()));
-  player = this.physics.add.sprite(20, 20, "tiles", 2);
+  visitors.children.iterate(v => (v.moved = Date.now() - 3000));
+
+  this.anims.create({
+    key: "walk_right",
+    frames: this.anims.generateFrameNumbers("player", {
+      start: 1,
+      end: 5
+    }),
+    repeat: -1,
+    frameRate: 10
+  });
+  this.anims.create({
+    key: "walk_left",
+    frames: this.anims.generateFrameNumbers("player", {
+      start: 6,
+      end: 11
+    }),
+    repeat: -1,
+    frameRate: 10
+  });
+  this.anims.create({
+    key: "idle",
+    frames: this.anims.generateFrameNumbers("player", { start: 0, end: 0 })
+  });
+  player = this.physics.add.sprite(40, 40, "player", 0);
   this.physics.add.collider(player, layer);
+  // this.physics.add.collider(player, visitors);
   this.physics.add.collider(visitors, layer);
   this.cameras.main.startFollow(player);
-  this.cameras.main.zoom = 1;
+  this.cameras.main.zoom = 4;
 
   text = this.add.text(16, 16, "Score: 0", {
     fontSize: "20px",
