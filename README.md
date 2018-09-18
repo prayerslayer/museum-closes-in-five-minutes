50d454691c26b3d2731ae661d0a73df89784a9c6
Nikolaus Piccolotto <nikolaus.piccolotto@artory.com>
2018-09-18 22:41:21 +0200

    Statemachine

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # museum-closes-in-five-minutes
 A game. Exit the museum, but see art on the way. Second attempt after prayerslayer/exit.
+
+## TODO
+
+* Pathfinding of visitors with constant speed
+* Score animation + character animation + text when painting was collected
+  * palim palim score
+  * "Wow, Schmerhard Trichter definitely outdid himself on that one."
+  * Jumping animation? not sure
+* Kicking animation
+* Up/down animation, possibly
+* Map needs doors
+* Build nice map
+* More artworks
--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
 import Phaser from "phaser";
 import sortBy from "lodash-es/sortBy";
-// import SPRITES from "./assets/img/exit_game.png";
 import PLAYER from "./assets/img/walk_right.png";
 import WALLS from "./assets/img/walls.png";
 import MUSEUM from "./assets/map/room.csv";
 import paintings from "./paintings";
+import { Machine } from "xstate";
 
 const SCALE_FACTOR = 4;
 const PLAYER_VELOCITY = 80 * SCALE_FACTOR;
 const VISITOR_VELOCITY = 20 * SCALE_FACTOR;
 
+const VisitorState = {
+  Idle: "Idle",
+  RandomlyMoving: "RandomlyMoving",
+  GoingToPainting: "GoingToPainting"
+};
+const VisitorAction = {
+  FindPainting: "FindPainting",
+  StartMoving: "StartMoving",
+  StopMoving: "StopMoving",
+  FoundPainting: "FoundPainting"
+};
+const Direction = {
+  GoLeft: "GoLeft",
+  GoRight: "GoRight",
+  GoUp: "GoUp",
+  GoDown: "GoDown"
+};
+const visitorFSM = Machine({
+  initial: VisitorState.Idle,
+  states: {
+    [VisitorState.Idle]: {
+      on: {
+        [VisitorAction.StartMoving]: VisitorState.RandomlyMoving,
+        [VisitorAction.FindPainting]: VisitorState.GoingToPainting
+      }
+    },
+    [VisitorState.RandomlyMoving]: {
+      on: {
+        [VisitorAction.StopMoving]: VisitorState.Idle
+      }
+    },
+    [VisitorState.GoingToPainting]: {
+      on: {
+        // Visitor will get to this fucking painting no matter what it takes
+        [VisitorAction.FoundPainting]: VisitorState.Idle
+      }
+    }
+  }
+});
 var config = {
   type: Phaser.AUTO,
   width: 640,
@@ -97,66 +136,91 @@ function updateText() {
   text.setText("Score: " + score);
 }
 
-const VisitorState = {
-  Idle: 1,
-  RandomlyMoving: 2,
-  GoingToPainting: 3
-};
+const visitorActions = [
+  VisitorAction.FindPainting,
+  VisitorAction.StartMoving,
+  VisitorAction.StopMoving
+];
 
-const visitorStateChanges = [0, 1, 2, 3, 4, 5, 6, 7];
+function visitorStateTransition(v) {
+  const currentState = v.getData("state");
+  if (Date.now() - v.getData("last_state_change") > 3000) {
+    const action = Phaser.Math.RND.weightedPick(visitorActions);
+    const newState = visitorFSM.transition(currentState, action).value;
+    return newState;
+  } else {
+    return v.getData("state");
+  }
+}
 
 function moveVisitors(dis) {
-  const now = Date.now();
   visitors.children.iterate(v => {
-    if (
-      v.getData("state") === VisitorState.Idle &&
-      now - v.getData("last_state_change") > 3000
-    ) {
-      const direction = Phaser.Math.RND.weightedPick(visitorStateChanges);
-      if (direction >= 4) {
-        const xDir = direction == 4 ? -1 : direction === 5 ? 1 : 0;
-        const yDir = direction == 6 ? -1 : direction === 7 ? 1 : 0;
-        v.setVelocityX(VISITOR_VELOCITY * xDir);
-        v.setVelocityY(VISITOR_VELOCITY * yDir);
-        if (xDir > 0) {
-          v.anims.play("walk_right", true);
-        } else if (xDir < 0) {
-          v.anims.play("walk_left", true);
-        }
-        v.setData({
-          state: VisitorState.RandomlyMoving,
-          last_state_change: Date.now()
-        });
-      } else if (direction === 0) {
-        const tile = Phaser.Math.RND.weightedPick(paintingTiles);
-        const { x, y } = layer.tileToWorldXY(tile.x, tile.y);
-        dis.tweens.add({
-          targets: v,
-          x: x + Phaser.Math.Between(0, 32),
-          y: y + 32,
-          onStart: () => v.anims.play("walk_right"),
-          onComplete: () => {
-            v.setData({
-              state: VisitorState.Idle,
-              last_state_change: Date.now()
+    const newState = visitorStateTransition(v);
+    const currentState = v.getData("state");
+    if (newState !== currentState) {
+      switch (newState) {
+        case VisitorState.Idle:
+          v.setVelocityX(0);
+          v.setVelocityY(0);
+          v.anims.play("idle", true);
+          break;
+        case VisitorState.GoingToPainting:
+          {
+            const tile = Phaser.Math.RND.weightedPick(paintingTiles);
+            const { x, y } = layer.tileToWorldXY(tile.x, tile.y);
+            dis.tweens.add({
+              targets: v,
+              x: x + Phaser.Math.Between(0, 32),
+              y: y + 32,
+              duration: 6000,
+              onStart: () => v.anims.play("walk_right"),
+              onComplete: () => {
+                const newState = visitorFSM.transition(
+                  VisitorState.GoingToPainting,
+                  VisitorAction.FoundPainting
+                ).value;
+                v.setData({
+                  state: newState,
+                  last_state_change: Date.now()
+                });
+                v.anims.play("idle");
+              }
             });
-            v.anims.play("idle");
           }
-        });
-        v.setData({
-          state: VisitorState.GoingToPainting,
-          last_state_change: Date.now()
-        });
-      } else {
-        v.setVelocityX(0);
-        v.setVelocityY(0);
-        v.anims.play("idle", true);
-        v.setData({
-          state: VisitorState.Idle,
-          last_state_change: Date.now()
-        });
+          break;
+        case VisitorState.RandomlyMoving:
+          {
+            const direction = Phaser.Math.RND.weightedPick(
+              Object.keys(Direction)
+            );
+            const xDir =
+              direction == Direction.GoLeft
+                ? -1
+                : direction === Direction.GoRight
+                  ? 1
+                  : 0;
+            const yDir =
+              direction == Direction.GoUp
+                ? -1
+                : direction === Direction.GoDown
+                  ? 1
+                  : 0;
+            v.setVelocityX(VISITOR_VELOCITY * xDir);
+            v.setVelocityY(VISITOR_VELOCITY * yDir);
+            if (xDir > 0) {
+              v.anims.play("walk_right", true);
+            } else if (xDir < 0) {
+              v.anims.play("walk_left", true);
+            }
+          }
+          break;
       }
+      v.setData({
+        state: newState,
+        last_state_change: Date.now()
+      });
     }
+
     v.depth = v.y;
   });
 }
@@ -186,9 +250,9 @@ function create() {
       tile.properties.visited = false;
       const { x, y } = layer.tileToWorldXY(tile.x, tile.y);
       const img = this.add.image(x + 8, y + 8, paintings[painting]);
-      // img.setOrigin(1, 1);
       img.setScale(SCALE_FACTOR);
       tile.properties.image = img;
+      // TODO need to check proper distribution around 50
       tile.properties.attractiveness = Phaser.Math.Between(0, 100);
       lastUsedPainting = painting;
       paintingTiles.push(tile);
@@ -209,7 +273,10 @@ function create() {
     visitors.get(x, y);
   }
   visitors.children.iterate(v => {
-    v.setData({ state: VisitorState.Idle, last_state_change: 0 });
+    v.setData({
+      state: visitorFSM.initialState.value,
+      last_state_change: Phaser.Math.Between(Date.now() - 3000, Date.now())
+    });
     v.setScale(SCALE_FACTOR);
   });
 
@@ -248,10 +315,6 @@ function create() {
   bg.setScrollFactor(0);
   bg.fillStyle(0xaaaaaa, 1);
   bg.fillRect(0, 0, 150, 24);
-  console.log(bg);
-  // bg.beginFill(0x000000, 1);
-  // bg.drawRect();
-  // bg.endFill();
 
   text = this.add.text(16, 16, "Score: 0", {
     fontSize: "20px",
